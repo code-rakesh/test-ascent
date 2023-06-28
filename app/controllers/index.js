38df993c83380553891180a361c881e62460de82
code-rakesh <code.rakeshkrishnan@gmail.com>
2023-06-28 05:18:52 -0700

    completed crud operations

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -18,7 +18,36 @@ const addSchedule = async (req, res) => {
         success: true, message: "welcome"
     })
 }
-
+const listSchedule = async (req, res) => {
+    const list = await Schedule.find({},{email:1,scheduledAt:1, message:1, name:1})
+    return res.status(200).json({
+        success: true, message: "welcome", list
+    })
+}
+const updateSchedule = async (req, res) => {
+    const id = req?.params.id
+    const { name, email, message, scheduledAtDay, scheduledAtTime } = req?.body
+    const exactTime = scheduledAtDay + "  " + scheduledAtTime
+    console.log(exactTime)
+    const milliseconds = new Date(exactTime).getTime()
+    const list = await Schedule.updateOne({_id:id},{name: name,
+        isDeleted: false,
+        email: email,
+        message: message,
+        scheduledAt: milliseconds,
+        didSend: false,
+        timeStamp: new Date().getTime()})
+    return res.status(201).json({
+        success: true, message: "welcome", list
+    })
+}
+const removeSchedule = async (req, res) => {
+    const id = req?.params.id
+    const list = await Schedule.updateOne({_id:id},{isDeleted:true})
+    return res.status(201).json({
+        success: true, message: "welcome", list
+    })
+}
 module.exports = {
-    addSchedule
+    addSchedule,listSchedule,updateSchedule, removeSchedule
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const {addSchedule} = require('../controllers/index')
+const {addSchedule,listSchedule,updateSchedule,removeSchedule} = require('../controllers/index')
 router.post('/test',addSchedule)
-
+router.get('/list',listSchedule)
+router.put('/update/:id',updateSchedule)
+router.delete('/delete/:id',removeSchedule)
 module.exports = router
